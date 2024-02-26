60812dce84cd0e6b16b2e0825f600cc304144305
Krihag <125972171+Krihag@users.noreply.github.com>
2024-02-27 00:59:31 +0100

    changes

--- src/js/components/post/comps/commentInput.js
+++ src/js/components/post/comps/commentInput.js
@@ -2,15 +2,17 @@ import requests from "../../../api/auth/requests/index.js";
 
 export default function commentInput(post) {
   const container = document.createElement("div");
-  container.className = "mt-4 flex items-center";
+  container.setAttribute("class", "mt-4 flex items-center");
 
   const input = document.createElement("textarea");
   input.placeholder = "Add a comment...";
-  input.className =
-    "flex-grow border p-2 rounded-md outline-none h-16 resize-none";
+  input.setAttribute(
+    "class",
+    "flex-grow border p-2 rounded-md outline-none h-16 resize-none"
+  );
 
   const button = document.createElement("button");
-  button.className = "ml-4 sm:ml-6";
+  button.setAttribute("class", "ml-4 sm:ml-6");
 
   const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
   svg.setAttributeNS(null, "fill", "none");
--- src/js/components/post/comps/likesAndComments.js
+++ src/js/components/post/comps/likesAndComments.js
@@ -62,7 +62,7 @@ export default function likesAndComments(post, fullPost = false) {
       "class",
       "text-sm font-medium md:text-base md:font-normal"
     );
-    container.setAttribute("class", "pr-12");
+    container.classList.add("pr-12");
     viewComments.append(viewCommentsText);
     container.appendChild(viewComments);
   }
