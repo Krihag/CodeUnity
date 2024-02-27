9a1c07c7f302e2376535c22c20380079110be7c9
Martine23FT <martinemidttveit@hotmail.com>
2024-02-27 15:08:10 +0100

    Moving files

--- src/js/pages/feed/index.js
+++ src/js/pages/feed/index.js
@@ -1,10 +1,7 @@
 import pageSpecific from "./pageSpecific.js";
-import explore from "./explore.js";
 
 await pageSpecific();
 console.log("test");
 window.onload = function () {
   console.log("test");
 };
-
-explore();
