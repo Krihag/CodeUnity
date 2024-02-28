7f7b8613006486770e7f99b44bf64e455deec628
Martine23FT <martinemidttveit@hotmail.com>
2024-02-28 14:28:19 +0100

    Changes

--- src/js/components/sidebar/mobileCloseOptions.js
+++
@@ -1,8 +0,0 @@
-export default function mobileCloseOptions() {
-    const closeIcon = document.querySelectorAll("#moreOptionsClose");
-    const optionsContainer = document.querySelector("#hamburgerOptions");
-    
-    closeIcon.addEventListener("click", () => {
-    optionsContainer.style.display = "hidden";
-    });
-}
--- src/js/components/sidebar/mobileToggleNav.js
+++ src/js/components/sidebar/mobileToggleNav.js
@@ -1,8 +1,21 @@
 import toggleContainer from "../dropdown/toggleContainer.js";
 
 export default function () {
-  const hiddenOptions = document.getElementById("hamburgerOptions");
   const hiddenOptionsBtn = document.getElementById("hamburgerMobile");
+  const hiddenOptions = document.getElementById("hamburgerOptions");
+
+  const toggleDropdown = () => {
+    if (hiddenOptions.style.display === "none") {
+      hiddenOptions.style.display = "block";
+    } else {
+      hiddenOptions.style.display = "none";
+    }
+  };
 
   toggleContainer(hiddenOptionsBtn, hiddenOptions, true);
+
+  const closeIcon = document.getElementById("moreOptionsClose");
+
+  closeIcon.addEventListener("click", toggleDropdown);
+  hiddenOptionsBtn.addEventListener("click", toggleDropdown);
 }
