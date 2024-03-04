10f8de63ac1b6a6a321caf81579f6d196b3ce0b3
Martine23FT <martinemidttveit@hotmail.com>
2024-03-04 21:07:22 +0100

    Update seePassword

--- src/js/pages/register/seePassword.js
+++ src/js/pages/register/seePassword.js
@@ -3,6 +3,16 @@ const passwordInput = document.getElementById("password");
 const confirmPassword = document.getElementById("confirmPassword");
 
 export default function seePassword() {
+  toggleButton.style.display = "none"; 
+
+  passwordInput.addEventListener("input", function() {
+    if (passwordInput.value.length > 0) {
+      toggleButton.style.display = "flex"; 
+    } else {
+      toggleButton.style.display = "none"; 
+    }
+  });
+
   toggleButton.addEventListener("click", function() {
     if (passwordInput.type === "password") {
       passwordInput.type = "text";
