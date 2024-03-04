4d6f8e10aa0e0e621bb24d3e4fc80b8165a790e1
Martine23FT <martinemidttveit@hotmail.com>
2024-03-04 23:11:12 +0100

    Loaders

--- src/js/pages/explore/index.js
+++ src/js/pages/explore/index.js
@@ -1,5 +1,10 @@
 import explore from "./explore.js";
 import pageSpefific from "./pageSpecific.js";
 
+const loadingIndicator = document.querySelectorAll(".loading-indicator");
+loadingIndicator.forEach(indicator => {
+    indicator.remove();
+});
+
 await pageSpefific();
 explore();
