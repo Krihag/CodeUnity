72f931242033c8efdf28dc9b8ffa6ec09d3419bc
Martine23FT <martinemidttveit@hotmail.com>
2024-03-05 17:27:02 +0100

    Changes

--- explore/backToTop.js
+++ explore/backToTop.js
@@ -1,6 +1,20 @@
 window.addEventListener('scroll', function() {
     var backButton = document.getElementById('backButton');
-    if (window.scrollY > 1100) { // Adjust the threshold as per your requirement
+    var breakpoint;
+  
+    if (window.innerWidth >= 1536) { 
+      breakpoint = 1600;
+    } else if (window.innerWidth >= 1280) { 
+      breakpoint = 1500;
+    } else if (window.innerWidth >= 768) { 
+      breakpoint = 2000;
+    } else if (window.innerWidth >= 640) { 
+      breakpoint = 2500;
+    } else { 
+      breakpoint = 3000;
+    }
+  
+    if (window.scrollY > breakpoint) {
       backButton.style.display = 'flex';
     } else {
       backButton.style.display = 'none';
